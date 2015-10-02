2279c467b676a4b7678575ab2a5663fb3954809c
hogelog <konbu.komuro@gmail.com>
2015-10-02 23:03:30 +0900

    Add rspec

---
+++ api/spec/requests/books_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Books", :type => :request do
+  fixtures :books
+
+  describe "books#index" do
+    it "returns books" do
+      get "/books"
+      expect(response).to have_http_status(200)
+      data = JSON.parse(response.body)
+      expect(data.size).to eq(2)
+      expect(data.map{|book| book["title"] }).to eq(%w(K&R Camel))
+    end
+  end
+
+  describe "books#show" do
+    it "returns book" do
+      get "/books/#{books(:c).id}"
+      expect(response).to have_http_status(200)
+      data = JSON.parse(response.body)
+      expect(data["title"]).to eq("K&R")
+    end
+  end
+end
